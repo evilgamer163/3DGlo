14a84d8caef8f1a7d1366465f5dc028f9920569f
evilgamer163 <evilgamestm@gmail.com>
2020-07-02 21:08:57 +0400

    Доделал слайдер. dot добавляются из js

--- js/main.js
+++ js/main.js
@@ -179,10 +179,25 @@ window.addEventListener('DOMContentLoaded', () => {
     const slider = () => {
         const slider = document.querySelector('.portfolio-content'),
             slide = document.querySelectorAll('.portfolio-item'),
-            dot = document.querySelectorAll('.dot'),
-            btns = document.querySelectorAll('.portfolio-btn');
+            portfolioDots = document.querySelector('.portfolio-dots');
 
-        let currentSlide = 0;
+        let currentSlide = 0,
+            interval,
+            dot;
+
+        const addDots = () => {
+            let arrayDots = [];
+            arrayDots.length = slide.length;
+            for(let i = 0; i < arrayDots.length; i++) {
+                arrayDots[i] = document.createElement('li');
+                arrayDots[i].classList.add('dot');
+                portfolioDots.append(arrayDots[i]);
+            }
+            dot = portfolioDots.querySelectorAll('.dot');
+            dot[0].classList.add('dot-active');
+        };
+
+        addDots();
 
         const next = (elem, index, strClass) => {
             elem[index].classList.add(strClass);
@@ -203,15 +218,61 @@ window.addEventListener('DOMContentLoaded', () => {
             next(dot, currentSlide, 'dot-active');
         };
 
-        const startSlider = (time) => {
-            setInterval(playSlide, time);
+        const startSlider = (time = 3000) => {
+            interval = setInterval(playSlide, time);
         };
 
         const stopSlider = () => {
-
+            clearInterval(interval);
         };
 
-        startSlider(10000);
+        slider.addEventListener('click', (event) => {
+            event.preventDefault();
+            let target = event.target;
+
+            if(!target.matches('.portfolio-btn, .dot')) {
+                return;
+            }
+
+            prev(slide, currentSlide, 'portfolio-item-active');
+            prev(dot, currentSlide, 'dot-active');
+
+            if(target.matches('#arrow-right')) {
+                currentSlide++;
+            } else if(target.matches('#arrow-left')) {
+                currentSlide--;
+            } else if(target.matches('.dot')) {
+                dot.forEach( (item, i) => {
+                    if(target === item) {
+                        currentSlide = i;
+                    }
+                });
+            }
+
+            if(currentSlide >= slide.length) {
+                currentSlide = 0;
+            }
+
+            if(currentSlide < 0) {
+                currentSlide = slide.length - 1;
+            }
+            next(slide, currentSlide, 'portfolio-item-active');
+            next(dot, currentSlide, 'dot-active');
+        });
+
+        slider.addEventListener('mouseover', (event) => {
+            if(event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+                stopSlider();
+            }
+        });
+
+        slider.addEventListener('mouseout', (event) => {
+            if(event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+                startSlider();
+            }
+        });
+
+        startSlider();
     };
     slider();
 });
