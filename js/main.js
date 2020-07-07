cf335908c3b7e426c210fee7933ccb4603d04a0d
evilgamer163 <evilgamestm@gmail.com>
2020-07-07 09:13:26 +0400

    Написал калькулятор

--- js/main.js
+++ js/main.js
@@ -296,8 +296,47 @@ window.addEventListener('DOMContentLoaded', () => {
     replacePhoto();
 
     //calc
-    const calculate = () => {
-        const calcInput = document.querySelectorAll('input[type="text"].calc-item');
+    const calculate = (price = 100) => {
+        const calcInput = document.querySelectorAll('input[type="text"].calc-item'),
+            calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcCount = document.querySelector('.calc-count'),
+            calcDay = document.querySelector('.calc-day'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1,
+                typeValye = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if(calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if(calcDay.value < 5 && calcDay.value) {
+                dayValue *= 2;
+            } else if(calcDay.value < 10 && calcDay.value) {
+                dayValue *= 1.5;
+            }
+
+            if(typeValye && squareValue) {
+                total = price * typeValye * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        };
+
+        calcBlock.addEventListener('change', (event) => {
+            let target = event.target;
+
+            if(target.matches('.calc-type') || target.matches('.calc-square')
+                || target.matches('.calc-day') || target.matches('.calc-count')) {
+                    countSum();
+                }
+        });
         
         calcInput.forEach( item => {
             item.addEventListener('input', () => {
